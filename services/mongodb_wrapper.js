6c59917a71118dfd395ccfe2cbcb1943854955e0
iamtheuserofthis <iamtheuserofthis@gmail.com>
2018-12-20 22:16:25 +0530

    fixed erroneous files

--- services/mongodb_wrapper.js
+++ services/mongodb_wrapper.js
@@ -4,46 +4,37 @@ const assert = require('assert');
 var url = 'mongodb://localhost:27017'
 var database = 'nodechat'
 
-var clientPromise = (url,dbname)=>{return MongoClient.connect(url,(err,client)=>{client.db(dbname)})}
-
-/*
-var insertUser = (colName, value)=>{
-   
-    const collection = db.collection(colName)
-    collection.insertOne({'name':document},(err,result)=>{
-        console.log(result)
+var clientPromise = (url,dbName)=>{
+  return new Promise((resolve, reject)=>{
+    MongoClient.connect(url,(err,client)=>{
+      if(err) reject(err);
+      else resolve(client.db(dbName));
     })
-    
+  })
 }
 
-insertUser('nodechat','Gaurav')
-*/
-clientPromise('mongodb://localhost:27017','nodechat').then(db=>{
-    const collection = db.collection('log_users')
-    collection.insertOne({'name':document},(err,result)=>{
-        console.log(result)
-    })
+
+clientPromise('mongodb://localhost:27017','nodechat')
+.then((db)=>{
+  console.log(db)
+})
+.catch(err=>{ 
+  console.log(err) 
 })
 
+var insertUser = (userdata, collectionName)=>{
+  clientPromise('mongodb://localhost:27017','nodechat')
+  .then(db=>{
+      const collection = db.collection(collectionName)
+      collection.insertOne({'name':userdata.username, 'email':userdata.email},(err,result)=>{
+        assert.equal(err,null)
+        console.log(result.result)
+      })
+  })
+}
 
+var k = {
+  username:'Garav',
+  email:'iamtheuserofthis@gmail.com'
+}
 
-/*
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
-  client.close();
-});
-
- collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-*/ 
