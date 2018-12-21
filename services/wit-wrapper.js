990c976e1c016bcbc78e65bd30417352d0871a91
iamtheuserofthis <iamtheuserofthis@gmail.com>
2018-12-21 18:08:37 +0530

    wit-wrapper changes

--- services/wit-wrapper.js
+++ services/wit-wrapper.js
@@ -1,4 +1,4 @@
-var db_connect = require('./postgres_wrapper')
+var db_connect = require('./mongodb_wrapper')
 
 const {Wit, log} = require('node-wit')
 
@@ -13,6 +13,16 @@ answer_questions = (msg)=>{
     return client.message(msg)   
 }
 
-var intents = answer_questions('Hi')
 
-console.log(intents)
+
+answer_questions('Tell me about documents').then((res)=>{
+    Object.keys(res.entities).forEach(element => {
+        console.log(element+'-'+res.entities[element][0].value)
+        
+    });
+
+    /*
+    console.log()
+    console.log(res.entities['intent'][0].value)
+    */
+})
