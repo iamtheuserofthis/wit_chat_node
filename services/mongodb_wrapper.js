33d524dba79d99832a7e30cc7c8b0d9d7a5417d8
iamtheuserofthis <iamtheuserofthis@gmail.com>
2018-12-21 15:08:10 +0530

    improvement in mongodb_wrapped.js

--- services/mongodb_wrapper.js
+++ services/mongodb_wrapper.js
@@ -14,13 +14,13 @@ var clientPromise = (url,dbName)=>{
 }
 
 
-clientPromise('mongodb://localhost:27017','nodechat')
-.then((db)=>{
-  console.log(db)
-})
-.catch(err=>{ 
-  console.log(err) 
-})
+// clientPromise('mongodb://localhost:27017','nodechat')
+// .then((db)=>{
+//   console.log(db)
+// })
+// .catch(err=>{ 
+//   console.log(err) 
+// })
 
 var insertUser = (userdata, collectionName)=>{
   clientPromise('mongodb://localhost:27017','nodechat')
@@ -33,8 +33,43 @@ var insertUser = (userdata, collectionName)=>{
   })
 }
 
-var k = {
-  username:'Garav',
-  email:'iamtheuserofthis@gmail.com'
+/*
+sample query and data from the 
+
+var udata = {
+  username:'Vinay Negi',
+  email:'vinaynegivins@gmail.com'
 }
 
+insertUser(udata, 'log_users') 
+
+
+*/
+
+
+
+var findAnswer = (intent_val, entities_val)=>{
+  clientPromise('mongodb://localhost:27017','nodechat')
+  .then(db=>{
+      const collection = db.collection('nodetest')
+      collection.findOne({
+        "intent":intent_val,
+        "entity":{
+        $elemMatch:entities_val
+      }}, (err,result)=>{
+          console.log(result.answer)
+        })
+  })
+}
+
+
+// db.nodetest.find( { "intent":"#get_info", "entity":{ $elemMatch:{ "exam":"star"}}},{"_id":0, "answer":1})
+
+
+/* this query works fine
+
+var intent = "#get_info"
+var entities = {"exam":"star"}
+findAnswer(intent,entities)
+
+*/
