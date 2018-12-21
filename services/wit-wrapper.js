0e62203b7f305cccc6683edaaedb33a908786eac
iamtheuserofthis <iamtheuserofthis@gmail.com>
2018-12-21 18:57:56 +0530

    minor changes

--- services/wit-wrapper.js
+++ services/wit-wrapper.js
@@ -15,14 +15,24 @@ answer_questions = (msg)=>{
 
 
 
+/* works but creating a json object is more helpful of mongodb query format
+
 answer_questions('Tell me about documents').then((res)=>{
     Object.keys(res.entities).forEach(element => {
-        console.log(element+'-'+res.entities[element][0].value)
+        console.log(element+':'+res.entities[element][0].value)
         
     });
+ })
+*/
 
-    /*
-    console.log()
-    console.log(res.entities['intent'][0].value)
-    */
-})
+
+answer_questions('Hi').then((res)=>{
+    var elems = new Object()
+    Object.keys(res.entities).forEach(element => {
+        elems[element] = res.entities[element][0].value
+    });
+    console.log(elems)
+ })
+
+
+
